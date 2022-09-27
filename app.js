903ffb74b0d466fc58f84059c2d2f4cf410ea337
Rayaa Ghirri <rghirri@gmail.com>
2022-09-27 14:52:32 +0100

    completed responsive gallery

--- app.js
+++ app.js
@@ -2,29 +2,29 @@
 //console.log(gallery);
 const galleryContainer = document.querySelector(".galleryContainer");
 
-function generateGallery(columns, galleryImages){
+function generateGallery(columns, galleryImages) {
     galleryContainer.innerHTML = '';
 
-    //console.log(galleryImages);
+    //     //console.log(galleryImages);
 
-    // Store all column arrays which contain the relevant gallery images
+    //     // Store all column arrays which contain the relevant gallery images
     let galleryColumnWrappers = {};
 
-    // create column item array and add this to colomnWrapper
-    for(let i =0; i < columns; i++) {
+    //     // create column item array and add this to colomnWrapper
+    for (let i = 0; i < columns; i++) {
         galleryColumnWrappers[`column${i}`] = [];
     }
 
-     //console.log(galleryImages.length);
+    //      //console.log(galleryImages.length);
 
-     //console.log(galleryColumnWrappers);
+    //      //console.log(galleryColumnWrappers);
 
-    for(let i = 0; i < galleryImages.length; i++) {
+    for (let i = 0; i < galleryImages.length; i++) {
         const column = i % columns;
         galleryColumnWrappers[`column${column}`].push(galleryImages[i]);
     }
-     // console.log(galleryColumnWrappers);
-    
+    //      // console.log(galleryColumnWrappers);
+
     for (let i = 0; i < columns; i++) {
         let columnGallaryImages = galleryColumnWrappers[`column${i}`];
         let columns = document.createElement('div');
@@ -37,15 +37,44 @@ function generateGallery(columns, galleryImages){
             let image = document.createElement('img');
             image.src = galleryImage.image;
             image.alt = galleryImage.caption;
-            
+
             let caption = document.createElement('figcaption');
             caption.innerText = galleryImage.caption;
             galleryFigure.append(image, caption);
             columns.appendChild(galleryFigure);
-        })
+        });
 
         galleryContainer.appendChild(columns);
     }
-      //console.log(columnGallaryImages);
+    //       //console.log(columnGallaryImages);
 }
+
 generateGallery(4, gallery);
+
+
+let previousScreenSize = window.innerWidth;
+
+//console.log(previousScreenSize);
+
+ window.addEventListener('resize', () => {
+    if (window.innerWidth < 600 && previousScreenSize >= 600) {
+        generateGallery(1, gallery);
+    }
+    else if (window.innerWidth >= 600 && window.innerWidth < 1000 && (previousScreenSize < 600 || previousScreenSize >= 1000)) {
+        generateGallery(2, gallery);
+    } else if (window.innerWidth >= 1000 && previousScreenSize < 1000) {
+        generateGallery(4, gallery);
+    }
+    previousScreenSize = window.innerWidth;
+ });
+
+// if(previousScreenSize < 600){
+//     generateGallery(1, gallery);
+// }else if(previousScreenSize >= 600 && previousScreenSize < 1000){
+//     generateGallery(2, gallery);
+// }else{
+//     generateGallery(4, gallery);
+// }
+
+
+
