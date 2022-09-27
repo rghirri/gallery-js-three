4455ced6046d26d51c3c2a92dfb41b6c63008b86
Rayaa Ghirri <rghirri@gmail.com>
2022-09-27 16:06:41 +0100

    update app.js index.html style.css

--- app.js
+++ app.js
@@ -1,103 +1,103 @@
-// asign elements
-//console.log(gallery);
-// const galleryHeader    = document.querySelector(".galleryHeader");
-const galleryContainer = document.querySelector(".galleryContainer");
-// Gallery Header
+// // asign elements
+// //console.log(gallery);
+// // const galleryHeader    = document.querySelector(".galleryHeader");
+// const galleryContainer = document.querySelector(".galleryContainer");
+// // Gallery Header
 
-//console.log(headerGallery);
-// function generateGalleryHeader(headerGallery) {
+// //console.log(headerGallery);
+// // function generateGalleryHeader(headerGallery) {
    
-//     headerGallery.forEach(header => {
-//         let headerOne = document.createElement('h1');
-//         headerOne.classList.add("galleryTitle");
-//         header.innerText = header.title;
+// //     headerGallery.forEach(header => {
+// //         let headerOne = document.createElement('h1');
+// //         headerOne.classList.add("galleryTitle");
+// //         header.innerText = header.title;
 
-//         let headerDiv = document.createElement('div');
-//         headerDiv.classList.add("gallerySubtitle");
+// //         let headerDiv = document.createElement('div');
+// //         headerDiv.classList.add("gallerySubtitle");
 
-//         let headerParagraph       = document.createElement('p');
-//         headerParagraph.innerText = header.subTitle;
-//         headerDiv.appendChild(headerParagraph);
-//     });
-//     galleryHeader.append(headerOne, headerDiv);
+// //         let headerParagraph       = document.createElement('p');
+// //         headerParagraph.innerText = header.subTitle;
+// //         headerDiv.appendChild(headerParagraph);
+// //     });
+// //     galleryHeader.append(headerOne, headerDiv);
 
+// // }
+// // generateGalleryHeader(headerGallery);
+
+// // Gallery Container
+// function generateGallery(columns, galleryImages) {
+//     galleryContainer.innerHTML = '';
+
+//     //     //console.log(galleryImages);
+
+//     //     // Store all column arrays which contain the relevant gallery images
+//     let galleryColumnWrappers = {};
+
+//     //     // create column item array and add this to colomnWrapper
+//     for (let i = 0; i < columns; i++) {
+//         galleryColumnWrappers[`column${i}`] = [];
+//     }
+
+//     //      //console.log(galleryImages.length);
+
+//     //      //console.log(galleryColumnWrappers);
+
+//     for (let i = 0; i < galleryImages.length; i++) {
+//         const column = i % columns;
+//         galleryColumnWrappers[`column${column}`].push(galleryImages[i]);
+//     }
+//     //      // console.log(galleryColumnWrappers);
+
+//     for (let i = 0; i < columns; i++) {
+//         let columnGallaryImages = galleryColumnWrappers[`column${i}`];
+//         let columns = document.createElement('div');
+//         columns.classList.add('galleyColumn');
+
+//         columnGallaryImages.forEach(galleryImage => {
+//             let galleryFigure = document.createElement('figure');
+//             // galleryFigure.attributes('tabindex', 0);
+//             // galleryFigure.attributes('role', "button");
+//             let image = document.createElement('img');
+//             image.src = galleryImage.image;
+//             image.alt = galleryImage.caption;
+
+//             let caption = document.createElement('figcaption');
+//             caption.innerText = galleryImage.caption;
+//             galleryFigure.append(image, caption);
+//             columns.appendChild(galleryFigure);
+//         });
+
+//         galleryContainer.appendChild(columns);
+//     }
+//     //       //console.log(columnGallaryImages);
 // }
-// generateGalleryHeader(headerGallery);
 
-// Gallery Container
-function generateGallery(columns, galleryImages) {
-    galleryContainer.innerHTML = '';
-
-    //     //console.log(galleryImages);
-
-    //     // Store all column arrays which contain the relevant gallery images
-    let galleryColumnWrappers = {};
-
-    //     // create column item array and add this to colomnWrapper
-    for (let i = 0; i < columns; i++) {
-        galleryColumnWrappers[`column${i}`] = [];
-    }
-
-    //      //console.log(galleryImages.length);
-
-    //      //console.log(galleryColumnWrappers);
-
-    for (let i = 0; i < galleryImages.length; i++) {
-        const column = i % columns;
-        galleryColumnWrappers[`column${column}`].push(galleryImages[i]);
-    }
-    //      // console.log(galleryColumnWrappers);
-
-    for (let i = 0; i < columns; i++) {
-        let columnGallaryImages = galleryColumnWrappers[`column${i}`];
-        let columns = document.createElement('div');
-        columns.classList.add('galleyColumn');
-
-        columnGallaryImages.forEach(galleryImage => {
-            let galleryFigure = document.createElement('figure');
-            // galleryFigure.attributes('tabindex', 0);
-            // galleryFigure.attributes('role', "button");
-            let image = document.createElement('img');
-            image.src = galleryImage.image;
-            image.alt = galleryImage.caption;
-
-            let caption = document.createElement('figcaption');
-            caption.innerText = galleryImage.caption;
-            galleryFigure.append(image, caption);
-            columns.appendChild(galleryFigure);
-        });
-
-        galleryContainer.appendChild(columns);
-    }
-    //       //console.log(columnGallaryImages);
-}
-
-generateGallery(4, gallery);
+// generateGallery(4, gallery);
 
 
-let previousScreenSize = window.innerWidth;
+// let previousScreenSize = window.innerWidth;
 
-//console.log(previousScreenSize);
+// //console.log(previousScreenSize);
 
- window.addEventListener('resize', () => {
-    if (window.innerWidth < 600 && previousScreenSize >= 600) {
-        generateGallery(1, gallery);
-    }
-    else if (window.innerWidth >= 600 && window.innerWidth < 1000 && (previousScreenSize < 600 || previousScreenSize >= 1000)) {
-        generateGallery(2, gallery);
-    } else if (window.innerWidth >= 1000 && previousScreenSize < 1000) {
-        generateGallery(4, gallery);
-    }
-    previousScreenSize = window.innerWidth;
- });
+//  window.addEventListener('resize', () => {
+//     if (window.innerWidth < 600 && previousScreenSize >= 600) {
+//         generateGallery(1, gallery);
+//     }
+//     else if (window.innerWidth >= 600 && window.innerWidth < 1000 && (previousScreenSize < 600 || previousScreenSize >= 1000)) {
+//         generateGallery(2, gallery);
+//     } else if (window.innerWidth >= 1000 && previousScreenSize < 1000) {
+//         generateGallery(4, gallery);
+//     }
+//     previousScreenSize = window.innerWidth;
+//  });
 
-// if(previousScreenSize < 600){
-//     generateGallery(1, gallery);
-// }else if(previousScreenSize >= 600 && previousScreenSize < 1000){
-//     generateGallery(2, gallery);
-// }else{
-//     generateGallery(4, gallery);
-// }
+// // if(previousScreenSize < 600){
+// //     generateGallery(1, gallery);
+// // }else if(previousScreenSize >= 600 && previousScreenSize < 1000){
+// //     generateGallery(2, gallery);
+// // }else{
+// //     generateGallery(4, gallery);
+// // }
 
 
 
